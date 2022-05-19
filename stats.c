9d90d4116c9d2d264f0316b4a658da3d4ee13cfc
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-05-19 12:16:37 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,6 +1,6 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+struct Stats compute_statistics(float* numberset, int setlength) {
     int index=0;
     float sum;
     struct Stats s;
