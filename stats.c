0e1ac33e078fa631267996046b493197fe9dd0dd
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-05-19 11:58:27 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,6 +5,44 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+   /* calling this function to sort float array from min to max range order */
+    numberset = compute_sort(numberset, setlength);
+    /* min value update */
+    s.min = numberset[index];
+    /* loop to find the sum */
+    for(;index<setlength;index++)
+    {
+        sum += numberset[index];
+    }
+    /* average calculation */
+    s.average = sum/setlength;
+    /* max value update */
+    s.max = numberset[index-1];
+    /* return the updated structure variable */
+    return s;
+}
+
+float* compute_sort(float* numset, int length)
+{
+    int loop_1=0, loop_2;
+    float temp_var=0;
+    /* outer loop using length based iteration */
+    for(;loop_1<length;loop_1++)
+    {
+        /* internal loop to compare each value with all array values */
+        for(loop_2=0;loop_2<length;loop_2++)
+        {
+            if(numset[loop_1]<numset[loop_2])
+            {
+                /* rearrange the array order when the condition is true */
+                temp_var = numset[loop_1];
+                numset[loop_1] = numset[loop_2];
+                numset[loop_2] = temp_var;
+            }
+        }
+    }
+    /* return the updated float value */
+    return numset;
 }
 
 int emailAlertCallCount = 0;
